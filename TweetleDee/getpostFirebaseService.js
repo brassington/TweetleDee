bb3584486f70e3c18e2b3d034320670e049eda27
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-15 11:55:15 -0500

    Added Sync Service with Firebase that runs every 15 seconds

---
+++ TweetleDee/getpostFirebaseService.js
@@ -0,0 +1,64 @@
+﻿// Firebase url: https://luminous-fire-7183.firebaseio.com/
+// This is a service that runs every 15 seconds to update sessionStorage and localStorage from firebase
+// You can add any object to sessionStorage and it will get sent the next time the service runs.
+// Read out the latest firebase objects from localStorage (anything written to localstorage will be erased
+//   every 15 seconds)
+
+//Function which handles sending new sessionStorage objects to Firebase
+var postTweet = function () {
+    for (var i = 0; i < sessionStorage.length; i++) {
+        var request = new XMLHttpRequest();
+        request.open("POST", "https://luminous-fire-7183.firebaseio.com/.json", true);
+        request.onload = function () {
+            if (this.status >= 200 && this.status < 400) {
+                //Successful
+                var data = JSON.parse(this.response);
+                console.log(data);
+                sessionStorage.clear();
+            } else {
+                //Request failed
+                console.log(this.response);
+            }
+        };
+        request.onerror = function () {
+            //Connection fails
+            console.log("Whoops, connection failed!");
+        };
+
+
+        request.send(sessionStorage[i]);
+    }
+}
+
+// Function which handles updating our localStorage from firebase every 15 seconds
+var getFireBaseData = function () {
+    var request = new XMLHttpRequest();
+    request.open("GET", "https://luminous-fire-7183.firebaseio.com/.json", true);
+    request.onload = function () {
+        if (this.status >= 200 && this.status < 400) {
+            //Successful
+            var data = JSON.parse(this.response);
+            localStorage.clear();
+            for (var propName in data) {
+                localStorage.setItem(propName, JSON.stringify(data[propName]))
+            }
+        } else {
+            //Request failed
+            console.log(this.response);
+        }
+    };
+    request.onerror = function () {
+        //Connection fails
+        console.log("Whoops, connection failed!");
+    };
+    request.send();
+}
+
+// Timer function which calls itself using setTimeout every 15 seconds. It sends objects from sessionStorage,
+// clears both session and local storage, then gets the latest objects from Firebase.
+var scheduleNextUpdate = function () {
+    postTweet();
+    getFireBaseData();
+    setTimeout(scheduleNextUpdate, 15000)
+}
+scheduleNextUpdate();
---
+++ TweetleDee/main.js
@@ -0,0 +1,28 @@
+﻿//New fake tweet that I'll keep sending in to firebase the first time the page loads.
+var tweet = {
+    name: "Andrew",
+    message: "I'm teaching an awesome class right now!"
+};
+
+// Example of adding something to session storage that will eventually get sent
+// to firebase when the FirebaseService kicks off every 15 seconds.
+if (sessionStorage.length) {
+    sessionStorage.setItem(JSON.stringify(sessionStorage[sessionStorage.length]), JSON.stringify(tweet));
+} else {
+    sessionStorage.setItem(0, JSON.stringify(tweet));
+}
+
+// Example of reading values out of session storage, then looping through localstorage next
+for (var propName in sessionStorage) {
+    document.getElementById("container").innerHTML +=
+    JSON.parse(sessionStorage[propName])["name"] +
+    ': ' +
+    JSON.parse(sessionStorage[propName])["message"] + "<br />";
+}
+// Now reading values out of localstorage, so that it will eventually get updated from firebase when the FirebaseService kicks off every 15 seconds.
+for (var propName in localStorage) {
+    document.getElementById("container").innerHTML +=
+    JSON.parse(localStorage[propName])["name"] +
+    ': ' +
+    JSON.parse(localStorage[propName])["message"] + "<br />";
+}
