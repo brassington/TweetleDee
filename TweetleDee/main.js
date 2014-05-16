58e5cde3790f889b1f29ff0d5ad47f2b0677af8c
J. Andrew Brassington <jabbrass@zoho.com>
2014-05-16 10:28:15 -0500

    Added selector shortcut in shortcuts.js

--- TweetleDee/main.js
+++ TweetleDee/main.js
@@ -1,4 +1,5 @@
 ﻿//New fake tweet that I'll keep sending in to firebase the first time the page loads.
+
 var tweet = {
     name: "Andrew",
     message: "I'm teaching an awesome class right now!"
@@ -22,7 +23,7 @@ if (sessionStorage.length) {
 
 // Now reading values out of localstorage, so that it will eventually get updated from firebase when the FirebaseService kicks off every 15 seconds.
 for (var propName in localStorage) {
-    document.getElementById("container").innerHTML +=
+    $("#container").innerHTML +=
     JSON.parse(localStorage[propName])["name"] +
     ': ' +
     JSON.parse(localStorage[propName])["message"] + "<br />";
